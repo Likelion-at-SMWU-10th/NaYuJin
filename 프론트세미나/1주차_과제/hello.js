95f2951953eaf2f78df7f703f35594f2cd176fbb
Nayujin <00nyj@sookmyung.ac.kr>
2022-05-23 18:21:24 +0900

    [5주차과제] js파일 함수이름과 인사문구 변경
    코드 이해 후 변경 완료

--- 프론트세미나/1주차_과제/hello.js
+++ 프론트세미나/1주차_과제/hello.js
@@ -2,11 +2,11 @@ const loginForm=document.querySelector("#login-form");
 const loginInput=document.querySelector("#login-form input");
 const hello=document.querySelector("#hello");
 
-function loginSubmit(event){
+function firstLogin(event){
     event.preventDefault();
     loginForm.classList.add("hidden");
     const userName=loginInput.value;
-    hello.innerText=`Hello ${userName}!`;
+    hello.innerText=`안녕하세요 ${userName}!`;
     hello.classList.remove("hidden");
 }
-loginForm.addEventListener("submit",loginSubmit);
+loginForm.addEventListener("submit",firstLogin);
