c102484fbdd43e397d7cfb5f33d371702b9522a3
YuJin Na <91943160+nayujin-dev@users.noreply.github.com>
2022-05-23 18:27:28 +0900

    [4주차과제] Security key 없애기
    장고 보안키 삭제

--- week4/myhw/myhw/settings.py
+++ week4/myhw/myhw/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o!tyzi+9alxnb6*kyp$^5(lpfl$vl6kc_r$1pqjf$pf*v(2_fg'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
